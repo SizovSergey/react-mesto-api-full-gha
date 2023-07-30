a4d00a9e0e86b84a9ef3ff8c0bbebcd980a3fe79
SergeySizov <vpnsizov@yandex.ru>
2023-07-30 20:57:05 +0300

    разбираюсь с cors2

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
