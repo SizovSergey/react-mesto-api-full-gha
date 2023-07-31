a2703263443d01ee4395956ebcf06a5ad9c1133b
Sergey Sizov <vpnsizov@yandex.ru>
2023-07-31 14:55:33 +0300

    подготовка

--- backend/middlewares/validate.js
+++ backend/middlewares/validate.js
@@ -11,7 +11,7 @@ const validateId = celebrate({
 const cardValidate = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(urlRegex),
   }),
 });
 
